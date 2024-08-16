917482e18fa23d035a91bf5c8f90a107a5385ba2
ParkByunggwon <startartart@naver.com>
2024-08-16 09:51:54 +0900

    chore: 라이브러리 추가 및 tailwind custom 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainDarkColor: '#0B52A9',
+        mainColor: '#366CF0',
+
+        mainBrightColor: '#EDEFF4',
+        mainBrighterColor: '#F5F6F9',
+
+        subColor: '#CDCED1',
+        subDarkColor: '#A5A6A9',
+      },
+      margin: {
+        header: '8vh',
+      },
+      // blur: {
+      //   xs: '1px',
+      // },
+    },
   },
   plugins: [],
 };
