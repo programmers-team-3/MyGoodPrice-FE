f3f9871aa076cb35fd766dd240fb1342b761fd13
ParkByunggwon <startartart@naver.com>
2024-08-16 09:59:34 +0900

    feat: 컴포넌트 구현 FilterBox, Modal, Overview, SearchBar

---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,29 @@
+import { ChangeEvent, useState } from 'react';
+import { IoIosSearch } from 'react-icons/io';
+type SearchBarProps = {
+  value: string;
+  handleChangeText: (text: string) => void;
+};
+export default function SearchBar({ value, handleChangeText }: SearchBarProps) {
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  return (
+    <div className="relative w-full rounded-l bg-mainBrightColor">
+      <input
+        type="text"
+        value={value}
+        className="appearance-none border rounded w-full p-2"
+        placeholder="가게 검색"
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          handleChangeText(e?.target.value)
+        }
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+      />
+      <IoIosSearch
+        className={`absolute right-4 top-1/2 transition duration-300
+      -translate-y-1/2 w-8 h-8 cursor-pointer
+      ${isFocused ? 'text-mainColor' : 'text-subDarkColor'}`}
+      />
+    </div>
+  );
+}
