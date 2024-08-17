ca4eb294203b274fbad0558f1c599ea63d1034a4
ParkByunggwon <startartart@naver.com>
2024-08-17 12:38:09 +0900

    style: theme 추가 및 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        mainDarkColor: '#0B52A9',
-        mainColor: '#366CF0',
+        mainDarkColor: "#0B52A9",
+        mainColor: "#366CF0",
 
-        mainBrightColor: '#EDEFF4',
-        mainBrighterColor: '#F5F6F9',
+        mainBrightColor: "#EDEFF4",
+        mainBrighterColor: "#F5F6F9",
 
-        subColor: '#CDCED1',
-        subDarkColor: '#A5A6A9',
+        subColor: "#CDCED1",
+        subDarkColor: "#A5A6A9",
       },
       margin: {
-        header: '8vh',
+        header: "8vh",
       },
-      // blur: {
-      //   xs: '1px',
-      // },
+      blur: {
+        xs: "2px",
+      },
     },
   },
   plugins: [],
