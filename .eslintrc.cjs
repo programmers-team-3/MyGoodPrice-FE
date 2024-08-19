7c26faf783b986999f7a1238b70387761851ca35
dah <cdh010126r@gmail.com>
2024-08-19 14:39:51 +0900

    fix: eslint 에러

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,42 @@
 module.exports = {
-    root: true,
-    env: { browser: true, es2020: true },
-    extends: [
-      'airbnb-typescript',
-      'airbnb/hooks',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:react-hooks/recommended',
-      'plugin:prettier/recommended',
-    ],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['react-refresh', '@typescript-eslint', 'prettier'],
-    rules: {
-      'react/react-in-jsx-scope': false,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      'prettier/prettier': 'error',
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module", // ES 모듈 사용
+    ecmaFeatures: {
+      jsx: true,
     },
-  };
-  
+  },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: "detect", // React 버전을 자동으로 감지
+    },
+  },
+  extends: [
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    "react/react-in-jsx-scope": "off", // React 17 이상에서는 JSX에서 React를 import할 필요 없음
+    "react/jsx-uses-react": "off", // JSX에서 React 사용을 감지하지 않게 함
+    "no-undef": "off", // TypeScript가 전역 변수를 정의하므로 no-undef 비활성화
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "prettier/prettier": "error",
+  },
+};
--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,27 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off", // React 17 이상에서는 JSX에서 React를 import할 필요 없음
+      "react/jsx-uses-react": "off", // JSX에서 React 사용을 감지하지 않게 함
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
