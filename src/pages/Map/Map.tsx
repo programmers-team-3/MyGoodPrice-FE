4dd1129f23a17718194c937b2aa9cf0a26b9654b
dah <cdh010126r@gmail.com>
2024-08-20 20:23:21 +0900

    feat: 카카오맵, 현위치

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -1,3 +1,22 @@
-export default function MapPage() {
-  return <h1 className="text-2xl">MapPage</h1>;
-}
+import KakaoMap from "@/components/Common/KakaoMap";
+import styled from "styled-components";
+
+const MapPage = () => {
+  return (
+    <MapPageStyle>
+      <KakaoMap />
+    </MapPageStyle>
+  );
+};
+
+const MapPageStyle = styled.div`
+  width: 50vw;
+  height: 100%;
+  margin: 0 auto;
+
+  @media (max-width: 768px) {
+    width: 100%; // 모바일 뷰 너비를 100%
+  }
+`;
+
+export default MapPage;
