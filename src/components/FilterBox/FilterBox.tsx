cd423e14ead2b8c2d719400ce9a48f8b307df95a
ParkByunggwon <startartart@naver.com>
2024-08-21 18:45:11 +0900

    feat: 필터링 구현 및 모달 리셋 버튼 추가 전역상태관리 X

--- src/components/FilterBox/FilterBox.tsx
+++ src/components/FilterBox/FilterBox.tsx
@@ -1,24 +1,47 @@
 import { Range, getTrackBackground } from "react-range";
 import { useState } from "react";
+import CityFilter from "./CityFilter";
+import CheckBox from "../CheckBox/CheckBox";
+import { theme } from "@/styles/theme";
+import Button from "../Button/Button";
+import { dummyFoodFilterData } from "@/store/data";
+import styled from "styled-components";
 
 const STEP = 100;
 const MIN = 0;
 const MAX = 10000;
 // 최솟값, 최댓값 수정 필요
 export default function FilterBox({ rtl }: { rtl: boolean }) {
-  const [values, setValues] = useState([0, 5000]);
+  const [values, setValues] = useState<number[]>([0, 10000]);
+  const [rangeFilter, setRangeFilter] = useState<boolean[]>([true, true, true]);
+  const [location, setLocation] = useState<string | null>(null);
+  const [category, setCategory] = useState<number | null>(1);
 
   const handleChange = (idx: number, value: string) => {
-    if (idx === 1 && Number(value) < values[0]) return;
-    if (idx === 0 && Number(value) > values[1]) return;
+    const numValue = Number(value);
+    // if (idx === 0 && numValue > values[1]) return;
+    // if (idx === 1 && numValue < values[0]) return;
+    if (numValue > 10000 || isNaN(numValue)) return;
     const updatedValues = [...values];
-    updatedValues[idx] = Number(value);
+    updatedValues[idx] = numValue;
     setValues(updatedValues);
   };
+
+  const handleRangeFilter = (idx: number = 1, isChecked: boolean) => {
+    const updatedFilter = [...rangeFilter];
+    updatedFilter[idx] = isChecked;
+    setRangeFilter(updatedFilter);
+  };
+
+  const handleSearch = () => {
+    console.log("search");
+  };
+
   return (
-    <>
+    <div className="flex flex-col gap-4 h-full pt-2">
       <div className="flex justify-center flex-wrap">
         <Range
+          disabled={!rangeFilter[0]}
           values={values}
           step={STEP}
           min={MIN}
@@ -31,7 +54,9 @@ export default function FilterBox({ rtl }: { rtl: boolean }) {
             <div
               onMouseDown={props.onMouseDown}
               onTouchStart={props.onTouchStart}
-              className=" w-full bg-subDarkColor"
+              className={` w-full ${
+                rangeFilter[0] ? "bg-mainDarkColor" : "bg-subDarkColor"
+              }`}
             >
               <div
                 ref={props.ref}
@@ -41,7 +66,11 @@ export default function FilterBox({ rtl }: { rtl: boolean }) {
                   borderRadius: "4px",
                   background: getTrackBackground({
                     values,
-                    colors: ["#ccc", "#548BF4", "#ccc"],
+                    colors: [
+                      theme.subColor,
+                      rangeFilter[0] ? theme.mainDarkColor : theme.subDarkColor,
+                      theme.subColor,
+                    ],
                     min: MIN,
                     max: MAX,
                     rtl,
@@ -57,29 +86,103 @@ export default function FilterBox({ rtl }: { rtl: boolean }) {
             <div
               {...props}
               key={props.key}
-              className="h-6 w-6 rounded-full bg-mainDarkColor"
+              className={`h-6 w-6 rounded-full ${
+                rangeFilter[0] ? "bg-mainDarkColor" : "bg-subDarkColor"
+              }`}
             ></div>
           )}
         />
       </div>
-      <div className="my-16 flex justify-center items-center gap-2">
-        <h3>금액 상한선</h3>
-        <input
-          type="number"
-          value={values[0]}
-          onChange={(e) => handleChange(0, e.target.value)}
-          className="w-24 px-3 py-2 border border-subDarkColor rounded-md
-          shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+      <div className="mt-8 flex justify-between items-center gap-2">
+        <p className="text-lg text-mainDarkColor font-bold whitespace-nowrap">
+          금액 상한선 필터
+        </p>
+        <CheckBox
+          isChecked={rangeFilter[0]}
+          id={0}
+          handleCheck={(isChecked: boolean) => handleRangeFilter(0, isChecked)}
         />
-        ~
-        <input
-          type="number"
-          value={values[1]}
-          onChange={(e) => handleChange(1, e.target.value)}
-          className="w-24 px-3 py-2 border border-subDarkColor rounded-md
-          shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+        <MobileLayout className="w-full flex gap-4 justify-end overflow-x-scroll">
+          <input
+            type="number"
+            disabled={!rangeFilter[0]}
+            value={values[0].toString()}
+            onChange={(e) => handleChange(0, e.target.value)}
+            className={`w-20 px-1 border border-subDarkColor ${
+              rangeFilter[0] ? "" : "text-subDarkColor"
+            } rounded focus:outline-none focus:ring-2 focus:ring-mainColor focus:border-transparent`}
+          />
+          ~
+          <input
+            type="number"
+            disabled={!rangeFilter[0]}
+            value={values[1].toString()}
+            onChange={(e) => handleChange(1, e.target.value)}
+            className={`w-20 px-1 border border-subDarkColor ${
+              rangeFilter[0] ? "" : "text-subDarkColor"
+            } rounded focus:outline-none focus:ring-2 focus:ring-mainColor focus:border-transparent`}
+          />
+        </MobileLayout>
+      </div>
+      <div className="mt-8 flex justify-between items-center gap-2">
+        <p className="text-lg text-mainDarkColor font-bold whitespace-nowrap">
+          시/도 필터
+        </p>
+        <CheckBox
+          isChecked={rangeFilter[1]}
+          id={1}
+          handleCheck={(isChecked: boolean) => handleRangeFilter(1, isChecked)}
+        />
+        <MobileLayout className="w-full flex justify-end">
+          {location ? location : "선택하지 않음"}
+        </MobileLayout>
+      </div>
+      {rangeFilter[1] && (
+        <CityFilter
+          handleLocationChange={(location: string | null) =>
+            setLocation(location)
+          }
+        />
+      )}
+      <div className="mt-8 flex justify-between items-center gap-2">
+        <p className="text-lg text-mainDarkColor font-bold whitespace-nowrap">
+          카테고리 필터
+        </p>
+        <CheckBox
+          isChecked={rangeFilter[2]}
+          id={2}
+          handleCheck={(isChecked: boolean) => handleRangeFilter(2, isChecked)}
+        />
+        <div className="w-full flex gap-2 overflow-x-scroll">
+          {rangeFilter[2] &&
+            dummyFoodFilterData.map((data) => {
+              return (
+                <Button
+                  key={data.id}
+                  isActive={category === data.id}
+                  size="small"
+                  name={data.name}
+                  handleSetCurrent={() => setCategory(data.id)}
+                />
+              );
+            })}
+        </div>
+      </div>
+
+      <div className="flex flex-col mt-auto mb-4">
+        <Button
+          isActive={true}
+          size="large"
+          name={"검색"}
+          handleSetCurrent={handleSearch}
         />
       </div>
-    </>
+    </div>
   );
 }
+
+const MobileLayout = styled.div`
+  @media (max-width: 542px) {
+    display: none;
+  }
+`;
--- src/components/Layout/ModalLayout.tsx
+++ src/components/Layout/ModalLayout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode, useEffect } from "react";
 import { createPortal } from "react-dom";
 import { IoIosClose } from "react-icons/io";
+import { GrPowerReset } from "react-icons/gr";
 import styled from "styled-components";
 
 type ModalProps = {
@@ -23,18 +24,33 @@ export default function Modal({
     };
   }, []);
 
+  const handleReset = () => {
+    console.log("handle Reset");
+    // zustand 상태 초기화
+  };
+
   return createPortal(
     <ModalLayout
       className="flex flex-col fixed right-1/2 top-0 translate-x-1/2
-    w-1/2 h-full z-10"
+    w-1/2 z-10"
     >
       {isOpen ? (
-        <div className="mx-12 border-y-2 border-subDarkColor">
+        <div className="mx-6 border-y-2 border-subDarkColor h-full">
+          <div
+            className="absolute left-10 top-4 flex gap-2 items-center cursor-pointer"
+            onClick={() => handleReset()}
+          >
+            <GrPowerReset
+              className="w-8 h-8
+            transition-transform duration-300 ease-in-out hover:rotate-90"
+            />
+            <p>초기화</p>
+          </div>
           <IoIosClose
             className="absolute right-8 w-16 h-16 cursor-pointer"
             onClick={() => setOpen(false)}
           />
-          <div className="my-header px-12 py-4">{children}</div>
+          <div className="mt-header pb-footer h-full">{children}</div>
         </div>
       ) : null}
     </ModalLayout>,
@@ -43,8 +59,8 @@ export default function Modal({
 }
 
 const ModalLayout = styled.div`
-  margin-top: 8vh;
-
+  margin: 8vh 0 10vh 0;
+  height: 82vh;
   @media (max-width: 768px) {
     width: 80%;
   }
