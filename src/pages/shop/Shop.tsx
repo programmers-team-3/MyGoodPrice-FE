c5ce834659170b4b94fb2b85925ef1f595b4d815
ParkByunggwon <startartart@naver.com>
2024-08-24 01:05:20 +0900

    feat: Shop 컴포넌트 API 연결

--- src/pages/shop/Shop.tsx
+++ src/pages/shop/Shop.tsx
@@ -1,3 +1,39 @@
+import { ShopTypes } from "@/types";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useSearchParams } from "react-router-dom";
+import useUserStore from "@/store/useUserStore";
+import Loading from "@/components/Loading/LoadingBar";
+
 export default function ShopPage() {
-  return <h1 className="text-2xl">ShopPage</h1>;
+  const [searchParams] = useSearchParams();
+  const [shopData, setShopData] = useState<ShopTypes | null>(null);
+  const { loading, setLoading } = useUserStore();
+
+  useEffect(() => {
+    const fetchShopData = async () => {
+      const id = searchParams.get("id");
+      if (id) {
+        try {
+          setLoading(true);
+          const url = `${
+            import.meta.env.VITE_PRODUCTION_API_BASE_URL
+          }/stores/${id}`;
+          const response = await axios.get(url);
+          console.log(response);
+          setShopData(response.data);
+          setLoading(false);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    };
+    fetchShopData();
+  }, [searchParams, setLoading]);
+
+  if (loading) return <Loading text="가게 정보 불러오는 중.." />;
+
+  return (
+    <div>{shopData ? <div>{shopData.name}</div> : <p>데이터 없음</p>}</div>
+  );
 }
